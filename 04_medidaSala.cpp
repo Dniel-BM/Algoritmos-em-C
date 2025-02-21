faa4db06b319f1f50c09acb5e9c8faf7485887fe
danielbm <137958150+Dniel-BM@users.noreply.github.com>
2025-02-21 10:04:17 -0400

    Add files via upload
    added file 04_medidaSala.cpp

---
+++ 04_medidaSala.cpp
@@ -0,0 +1,28 @@
+/*
+04_medidaSala.cpp
+
+Descricao:Dadas as medidas de uma sala em metros (comprimento e largura), informe a sua área em metros quadrados.
+
+Fonte: Silvio Do Lago Pereira. Algoritmos e Logica de Programacao em C – Uma Abordagem Didatica (Portuguese Edition) (p. 24). Editora Erica | Saraiva. Edicao do Kindle.
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+    float larg, comp, area;
+
+    printf("Largura: ");
+    scanf("%f", &larg);
+
+    printf("Comprimento: /n");
+    scanf("%f", &comp);
+
+    area = comp*larg;
+    printf("Area da sala: %.2f metros quadrados\n\n\n", area);
+
+    system("pause");
+    return 0;
+}
