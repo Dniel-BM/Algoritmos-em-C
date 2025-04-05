b138ad53b6bffc00572e7b3a607809e7390a1d8d
danielbm <137958150+Dniel-BM@users.noreply.github.com>
2025-04-05 17:35:44 -0300

    Add files via upload

---
+++ Exercicio 1-6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/*
+06_dolarToReal
+
+Dados um valor em real e a cotação do dólar, converta esse valor em dólares.
+
+nivel: elementar
+
+Fonte:
+Silvio Do Lago Pereira. Algoritmos e Lógica de Programação em C – Uma Abordagem Didática (Portuguese Edition) (p. 24). Editora Érica | Saraiva. Edição do Kindle.
+ */
+
+int main(){
+	float real;
+	float dolar;
+	float conversao;
+
+    printf("Digite a cotacao atual do dolar: ");
+    scanf("%f", &dolar);
+
+    printf("Digite o saldo em Real: ");
+    scanf("%f", &real);
+
+	conversao = real / dolar;
+	printf("O saldo convertido para dolar e: %.2f", conversao);
+	
+	return 0;
+}
